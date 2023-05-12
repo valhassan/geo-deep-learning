08dfa4ee115930ade48369b07b5c40814efdef23
valhassan <victor.alhassan@canada.ca>
2023-05-11 23:56:09 -0400

    load image and labels outside __getitem__ method

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -2,13 +2,15 @@ import os
 import h5py
 import rasterio
 import numpy as np
+import pandas as pd
+
 from pathlib import Path
 from rasterio.plot import reshape_as_image
 from omegaconf import OmegaConf, DictConfig
 from torch.utils.data import Dataset, Sampler
+from typing import List
 
 from utils.logger import get_logger
-from utils.utils import ordereddict_eval
 
 # These two import statements prevent exception when using eval(metadata) in SegmentationDataset()'s __init__()
 from rasterio.crs import CRS
@@ -17,49 +19,6 @@ from affine import Affine
 # Set the logging file
 logging = get_logger(__name__)  # import logging
 
-
-def append_to_dataset(dataset, sample):
-    """
-    Append a new sample to a provided dataset. The dataset has to be expanded before we can add value to it.
-    :param dataset:
-    :param sample: data to append
-    :return: Index of the newly added sample.
-    """
-    old_size = dataset.shape[0]  # this function always appends samples on the first axis
-    dataset.resize(old_size + 1, axis=0)
-    dataset[old_size, ...] = sample
-    return old_size
-
-
-def create_files_and_datasets(samples_size: int, number_of_bands: int, samples_folder: Path, cfg: DictConfig):
-    """
-    Function to create the hdfs files (trn, val and tst).
-    :param samples_size: size of individual hdf5 samples to be created
-    :param number_of_bands: number of bands in imagery
-    :param samples_folder: (str) Path to the output folder.
-    :param cfg: (dict) Parameters found in the yaml config file.
-    :return: (hdf5 datasets) trn, val ant tst datasets.
-    """
-    real_num_bands = number_of_bands
-    assert real_num_bands > 0, "invalid number of bands when accounting for meta layers"
-    hdf5_files = []
-    for subset in ["trn", "val", "tst"]:
-        hdf5_file = h5py.File(os.path.join(samples_folder, f"{subset}_samples.hdf5"), "w")
-        hdf5_file.create_dataset("sat_img", (0, samples_size, samples_size, real_num_bands), np.uint16,
-                                 maxshape=(None, samples_size, samples_size, real_num_bands))
-        hdf5_file.create_dataset("map_img", (0, samples_size, samples_size), np.int16,
-                                 maxshape=(None, samples_size, samples_size))
-        hdf5_file.create_dataset("meta_idx", (0, 1), dtype=np.int16, maxshape=(None, 1))
-        try:
-            hdf5_file.create_dataset("metadata", (0, 1), dtype=h5py.string_dtype(), maxshape=(None, 1))
-            hdf5_file.create_dataset("sample_metadata", (0, 1), dtype=h5py.string_dtype(), maxshape=(None, 1))
-            hdf5_file.create_dataset("params", (0, 1), dtype=h5py.string_dtype(), maxshape=(None, 1))
-            append_to_dataset(hdf5_file["params"], repr(OmegaConf.create(OmegaConf.to_yaml(cfg, resolve=True))))
-        except AttributeError:
-            logging.exception(f'Update h5py to version 2.10 or higher')
-            raise
-        hdf5_files.append(hdf5_file)
-    return hdf5_files
 
 class SegmentationDataset(Dataset):
     """Semantic segmentation dataset based on HDF5 parsing."""
@@ -85,48 +44,37 @@ class SegmentationDataset(Dataset):
         self.dontcare = dontcare
         self.list_path = dataset_list_path
         self.parent_folder = dataset_list_path.parent
+        
         if not Path(self.list_path).is_file():
             logging.error(f"Couldn't locate dataset list file: {self.list_path}.\n"
                           f"If purposely omitting test set, this error can be ignored")
-            self.max_sample_count = 0
-        else:
-            with open(self.list_path, 'r') as datafile:
-                datalist = datafile.readlines()
-                if self.max_sample_count is None:
-                    self.max_sample_count = len(datalist)
+        
+        self.assets = self._load_data()
 
     def __len__(self):
-        return self.max_sample_count
+        return len(self.assets)
 
     def __getitem__(self, index):
-        with open(self.list_path, 'r') as datafile:
-            datalist = datafile.readlines()
-            data_line = datalist[index]
-            sat_file = self.parent_folder / data_line.split(';')[0]
-            label_file = self.parent_folder / data_line.split(';')[1].rstrip('\n')
-            with rasterio.open(sat_file, 'r') as sat_handle:
-                sat_img = reshape_as_image(sat_handle.read())
-                metadata = sat_handle.meta
-            with rasterio.open(label_file, 'r') as label_handle:
-                map_img = reshape_as_image(label_handle.read())
-                map_img = map_img[..., 0]
+        
+        sat_img, metadata = self._load_image(index)
+        map_img = self._load_label(index)
 
-            assert self.num_bands <= sat_img.shape[-1]
-
-            if isinstance(metadata, np.ndarray) and len(metadata) == 1:
-                metadata = metadata[0]
-            elif isinstance(metadata, bytes):
-                metadata = metadata.decode('UTF-8')
-            try:
-                metadata = eval(metadata)
-            except TypeError:
-                pass
+        if isinstance(metadata, np.ndarray) and len(metadata) == 1:
+            metadata = metadata[0]
+        elif isinstance(metadata, bytes):
+            metadata = metadata.decode('UTF-8')
+        try:
+            metadata = eval(metadata)
+        except TypeError:
+            pass
 
         sample = {"sat_img": sat_img, "map_img": map_img, "metadata": metadata, "list_path": self.list_path}
-
-        if self.radiom_transform:  # radiometric transforms should always precede geometric ones
+        # radiometric transforms should always precede geometric ones
+        if self.radiom_transform:  
             sample = self.radiom_transform(sample)
-        if self.geom_transform:  # rotation, geometric scaling, flip and crop. Will also put channels first and convert to torch tensor from numpy.
+        # rotation, geometric scaling, flip and crop. 
+        # Will also put channels first and convert to torch tensor from numpy.
+        if self.geom_transform:
             sample = self.geom_transform(sample)
         sample = self.totensor_transform(sample)
 
@@ -146,7 +94,53 @@ class SegmentationDataset(Dataset):
         sample['index'] = index
 
         return sample
+    
+    def _load_data(self) -> List[str]:
+        """Load the filepaths to images and labels
+        
+        Returns:
+            List[str]: a list of filepaths to train/test data  
+        """
+        df = pd.read_csv(self.list_path, sep=';', header=None, usecols=[i for i in range(2)])
+        assets = [{"image": x, "label": y} for x, y in zip(df[0], df[1])]
+        
+        return assets
+    
+    def _load_image(self, index: int):
+        """ Load image 
 
+        Args:
+            index: poosition of image
+
+        Returns:
+            image array and metadata
+        """
+        image_path = self.parent_folder / self.assets[index]["image"]
+        with rasterio.open(image_path, 'r') as image_handle:
+                image = reshape_as_image(image_handle.read())
+                metadata = image_handle.meta
+        assert self.num_bands <= image.shape[-1]
+        
+        return image, metadata
+    
+    def _load_label(self, index: int):
+        """ Load label 
+
+        Args:
+            index: poosition of label
+
+        Returns:
+            label array and metadata
+        """
+        label_path = self.parent_folder / self.assets[index]["label"]
+        
+        with rasterio.open(label_path, 'r') as label_handle:
+                label = reshape_as_image(label_handle.read())
+                label = label[..., 0]
+        
+        return label
+        
+        
 
 class DatasetFromSampler(Dataset):
     """Dataset to create indexes from `Sampler`.
