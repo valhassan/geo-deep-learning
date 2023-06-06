41cd04515499410fc1f1d1f78e9c2c669486876d
valhassan <victor.alhassan@canada.ca>
2023-06-06 19:13:59 -0400

    change torch precision and deterministic defaults

--- train_segmentation.py
+++ train_segmentation.py
@@ -29,8 +29,9 @@ def seed_everything(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
+    torch.set_float32_matmul_precision("high")
 
 
 seed_everything(42)
