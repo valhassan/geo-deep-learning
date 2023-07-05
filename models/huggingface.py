943461025e3f6745ce12b09500570a29095ad710
valhassan <victor.alhassan@canada.ca>
2023-07-05 11:00:25 -0400

    added upernet

--- models/huggingface.py
+++ models/huggingface.py
@@ -2,7 +2,7 @@ import torch
 import logging
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import SegformerForSemanticSegmentation
+from transformers import SegformerForSemanticSegmentation, UperNetForSemanticSegmentation
 
 logging.getLogger(__name__)
 
@@ -23,3 +23,20 @@ class SegFormer(nn.Module):
         upsampled_logits = F.interpolate(input=logits, size=img.shape[2:], 
                                          scale_factor=None, mode='bilinear', align_corners=False)
         return upsampled_logits
+    
+    
+class Upernet(nn.Module):
+    def __init__(self, model_name, in_channels, classes) -> None:
+        super().__init__()
+        
+        if in_channels != 3:
+            logging.critical(F"Upernet model expects three channels input")
+        
+        self.model = UperNetForSemanticSegmentation.from_pretrained(model_name,
+                                                                    num_labels=classes,
+                                                                    ignore_mismatched_sizes=True)
+    
+    def forward(self, img):
+        output = self.model(img)
+        logits = output.logits
+        return logits
