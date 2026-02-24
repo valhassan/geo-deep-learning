62e1c2949e95d4bdc21da878d6163fe33b55f23a
valhassan <victor.alhassan@canada.ca>
2026-02-24 13:42:31 -0500

    Refactor GeoAware Loss to replace Lovasz-Softmax with Dice Loss for improved performance in semantic segmentation. Adjust alpha parameter and enhance ignore index handling for better flexibility in loss computation.

--- geo_deep_learning/tools/losses/geoloss.py
+++ geo_deep_learning/tools/losses/geoloss.py
@@ -1,8 +1,7 @@
 """
 GeoAware Loss for semantic segmentation.
 
-Combines Lovasz-Softmax for overall region IoU with
-Boundary F1 Loss specifically targeting sharp geometries.
+Combines Dice loss with Boundary F1 Loss specifically targeting sharp geometries.
 """
 
 import segmentation_models_pytorch as smp
@@ -18,10 +17,10 @@ class GeoAwareLoss(nn.Module):
     def __init__(
         self,
         classes: list[int] | None = None,
-        alpha: float = 0.5,
+        alpha: float = 0.2,
         theta0: int = 3,
         theta: int = 5,
-        ignore_index: int = 255,
+        ignore_index: int | None = 255,
     ) -> None:
         """
         Initialize GeoAware Loss.
@@ -31,17 +30,17 @@ class GeoAwareLoss(nn.Module):
             alpha: weight multiplier for the boundary loss
             theta0: kernel size for the boundary loss
             theta: kernel size for the boundary loss
-            ignore_index: index of the ignored class
+            ignore_index: index of the ignored class, None for no ignored class
 
         """
         super().__init__()
         self.alpha = alpha
         self.ignore_index = ignore_index
 
-        # Region Loss: Handles the holistic geometry of all 5 classes
-        self.region_loss = smp.losses.LovaszLoss(
+        # Region Loss: Handles the holistic geometry over all classes
+        self.region_loss = smp.losses.DiceLoss(
             mode="multiclass",
-            per_image=False,
+            smooth=1e-5,
             from_logits=True,
             ignore_index=self.ignore_index,
         )
@@ -66,6 +65,14 @@ class GeoAwareLoss(nn.Module):
 
         """
         l_region = self.region_loss(pred, gt)
-        l_boundary = self.boundary_loss(pred, gt)
+
+        gt_b = gt.clone()
+        ignore_mask = None
+
+        if self.ignore_index is not None:
+            ignore_mask = (gt != self.ignore_index).float()
+            gt_b[gt_b == self.ignore_index] = 0
+
+        l_boundary = self.boundary_loss(pred, gt_b, ignore_mask)
 
         return l_region + (self.alpha * l_boundary)
