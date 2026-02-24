1f8829215c8cd26444c2a2f5314a1f3538da130a
valhassan <victor.alhassan@canada.ca>
2026-02-24 13:43:02 -0500

    minor change.

--- infer.sh
+++ infer.sh
@@ -37,9 +37,9 @@ kill_existing_processes
 show_gpu_status
 
 python $INFERENCE_SCRIPT \
-    --checkpoint /export/sata01/wspace/test_dir/multi/all_rgb_data/static_segformer_dim_768.ckpt \
-    --input /home/valhassa/Projects/geo-deep-learning/data/NS2-058651012010_01_P001-WV02-RGB.tif \
-    --output /home/valhassa/Projects/geo-deep-learning/data/out/ns2_wv2_static_segformer_rgb.tif \
+    --checkpoint /export/sata01/wspace/test_dir/multi/all_rgb_data/geoaware_epoch_45_val_loss_0.527.ckpt \
+    --input /home/valhassa/Projects/geo-deep-learning/experiments/data/NS2-058651012010_01_P001-WV02-RGB.tif \
+    --output /home/valhassa/Projects/geo-deep-learning/experiments/data/out/ns2_wv2_static_segformer_rgb_geoaware.tif \
     --mean 0.1014 0.1360 0.1296 \
     --std 0.1102 0.1230 0.1107
     # --mean 0.405 0.432 0.397 \
