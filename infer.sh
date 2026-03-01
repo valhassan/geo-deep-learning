2af1b0b894c48276cfe0c7db2b87ac80f59d7eef
valhassan <victor.alhassan@canada.ca>
2026-03-01 12:17:24 -0500

    parameter change.

--- infer.sh
+++ infer.sh
@@ -41,21 +41,21 @@ kill_existing_processes() {
 kill_existing_processes
 show_gpu_status
 
-python -m geo_deep_learning.tools.patch_checkpoint \
-  /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_epoch_47_val_loss_0.599.ckpt \
-  -o /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_patched.ckpt \
-  --no-weights-only
+# python -m geo_deep_learning.tools.patch_checkpoint \
+#   /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_epoch_47_val_loss_0.599.ckpt \
+#   -o /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_patched.ckpt \
+#   --no-weights-only
 
-python -m geo_deep_learning.tools.export_model \
-  --model dofa \
-  --checkpoint /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_patched.ckpt \
-  --output dofav2_geoaware_patched.pt2
+# python -m geo_deep_learning.tools.export_model \
+#   --model dofa \
+#   --checkpoint /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_patched.ckpt \
+#   --output dofav2_geoaware_patched.pt2
 
 
 python $INFERENCE_SCRIPT \
-    --checkpoint dofav2_geoaware_patched.pt2 \
+    --checkpoint /export/sata01/wspace/test_dir/multi/all_rgb_data/dofav2_geoaware_epoch_25_val_loss_0.615.ckpt \
     --input /home/valhassa/Projects/geo-deep-learning/experiments/data/NS2-058651012010_01_P001-WV02-RGBN.tif \
-    --output /home/valhassa/Projects/geo-deep-learning/experiments/data/out/ns2_wv2_dofav2_geoaware_exported_0.599_test.tif \
+    --output /home/valhassa/Projects/geo-deep-learning/experiments/data/out/ns2_wv2_dofav2_geoaware_epoch_25_val_loss_0.615_test.tif \
     --mean 0.1014 0.1360 0.1296 0.2604 \
     --std 0.1102 0.1230 0.1107 0.2099 \
     --wavelengths 0.6599999999999999 0.5449999999999999 0.48 0.8325 \
