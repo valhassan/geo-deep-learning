92fd61a3a74d74e418a942ef7d17b4526b75f0a8
valhassan <victorlazio109@yahoo.com>
2026-09-26 16:04:04 +0000

    Refactor SegFormerSegmentationModel to integrate WavelengthMixTransformer.

--- geo_deep_learning/models/segmentation/segformer.py
+++ geo_deep_learning/models/segmentation/segformer.py
@@ -4,10 +4,8 @@ import torch
 import torch.nn.functional as fn
 
 from geo_deep_learning.models.decoders.segformer_mlp import Decoder
-from geo_deep_learning.models.encoders.mix_transformer import (
-    DynamicMixTransformer,
-    get_encoder,
-)
+from geo_deep_learning.models.encoders.mix_transformer import get_encoder
+from geo_deep_learning.models.encoders.wavelength_stem import WavelengthMixTransformer
 from geo_deep_learning.models.heads.segmentation_head import SegmentationOutput
 
 from .base import BaseSegmentationModel
@@ -29,11 +27,14 @@ class SegFormerSegmentationModel(BaseSegmentationModel):
     ) -> None:
         """Initialize SegFormer segmentation model."""
         super().__init__()
+        self.use_dynamic_encoder = use_dynamic_encoder
+        skip_channels = None
         if use_dynamic_encoder:
-            self.encoder = DynamicMixTransformer(
+            self.encoder = WavelengthMixTransformer(
                 encoder=encoder,
                 weights=weights,
             )
+            skip_channels = self.encoder.stem.embed_dim
         else:
             self.encoder = get_encoder(
                 name=encoder,
@@ -48,13 +49,25 @@ class SegFormerSegmentationModel(BaseSegmentationModel):
             encoder=encoder,
             num_classes=num_classes,
             embedding_dim=embedding_dim,
+            skip_channels=skip_channels,
         )
         self.output_struct = SegmentationOutput
 
-    def forward(self, img: torch.Tensor) -> SegmentationOutput:
-        """Forward pass."""
-        x = self.encoder(img)
-        out, aux = self.decoder(x)
+    def forward(
+        self,
+        img: torch.Tensor,
+        wavelengths: torch.Tensor | None = None,
+    ) -> SegmentationOutput:
+        """Forward pass. Wavelengths are required for the dynamic stem."""
+        if self.use_dynamic_encoder:
+            if wavelengths is None:
+                msg = "wavelengths are required for the wavelength stem"
+                raise ValueError(msg)
+            feats, skip = self.encoder(img, wavelengths)
+            encoded = [skip, *feats]
+        else:
+            encoded = self.encoder(img)
+        out, aux = self.decoder(encoded)
         out = fn.interpolate(
             input=out,
             size=img.shape[2:],
